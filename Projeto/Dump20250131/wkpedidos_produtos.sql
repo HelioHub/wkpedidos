f501e5c197a872e32918780aa400f83118cc4977
HelioHub <helio111171souza@gmail.com>
2025-02-01 06:22:42 -0300

    Ajustes nas Pastas do Projeto

---
+++ Projeto/Dump20250131/wkpedidos_produtos.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wkpedidos
+-- ------------------------------------------------------
+-- Server version	5.7.44-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `CodigoProdutos` int(11) NOT NULL AUTO_INCREMENT,
+  `DescricaoProdutos` varchar(80) DEFAULT NULL,
+  `PrecoVendaProdutos` decimal(17,3) DEFAULT NULL,
+  PRIMARY KEY (`CodigoProdutos`),
+  KEY `INDEX_DESCRICAO` (`DescricaoProdutos`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (1,'Mouse',15.000),(2,'Monitor 14p',200.000),(3,'CPU Intel i5',2000.000),(4,'Teclado',25.000),(5,'Placa-Mãe X',500.000),(6,'Placa-Mãe Y',600.000),(7,'Placa-Mãe W',700.000),(8,'Placa-Vídeo X',300.000),(9,'Placa-Vídeo Y',400.000),(10,'Placa-Vídeo W',500.000),(11,'Processador i3',600.000),(12,'Processador i5',800.000),(13,'Processador i9',1000.000),(14,'CPU i3',1000.000),(15,'CPU i9',2000.000),(16,'Gabinete X',90.000),(17,'Gabinete Y',100.000),(18,'Gabinete W',110.000),(19,'Camera HD',100.000),(20,'Pad-Mouse',5.500);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-31 16:04:28
